ecfe594e16d16bf600cf31edd456859bfd2f6f62
alex <alexmuthini3@gmail.com>
2023-04-24 20:57:03 +0300

    corrected pycodestyle errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     userId = sys.argv[1]
 
     user = requests.get(base_url + "users/{}".format(userId)).json()
-    todos = requests.get(base_url + "todos", params={'userId':userId}).json()
+    todos = requests.get(base_url + "todos", params={'userId': userId}).json()
 
     completed_todos = [todo.get('title') for todo in todos
                        if todo.get('completed') is True]
-    print("Employee {} is done with tasks({}/{})".format(user.get('name'),
-           len(completed_todos), len(todos)))
+    print("Employee {} is done with tasks({}/{})"
+          .format(user.get('name'), len(completed_todos), len(todos)))
     for todo in completed_todos:
         print("\t {}".format(todo))

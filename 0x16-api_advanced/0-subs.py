f96a68cf8b081f1e07109b5ac5184ba773746813
Alecky3 <alexmuthini3@gmail.com>
2023-08-09 13:13:33 +0300

    modified subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 contains one function that returns the number of subscribers for a subreddit
-"""
+'''
 import requests
 
 
@@ -10,13 +10,13 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     """
     headers = {
-        'User-Agent': 'Windows:alx_scripts_python:v1.0.0 (/u/Alecky3)'
+        'User-Agent': 'Windows:alx_scripts'
     }
-    res = requests.get(
-        'https://www.reddit.com/r/{}/about/.json'.format(subreddit),
+    res = requests.get('https://www.reddit.com/r/{}/about/.json'.format(
+        subreddit),
         headers=headers,
         allow_redirects=False
-    )
+        )
     if res.status_code == 200:
         return res.json()['data']['subscribers']
     return 0

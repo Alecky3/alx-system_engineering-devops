005a7a8ff38cea677b421d2c4c4e80ca2cd5843a
Alecky3 <alexmuthini3@gmail.com>
2023-08-09 12:55:29 +0300

    modified user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 contains one function that returns the number of subscribers for a subreddit
-'''
+"""
 import requests
 
 
@@ -10,13 +10,7 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     """
     api_headers = {
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
+        'User-Agent': 'Windows:alx_scripts_python:v1.0.0 (/u/Alecky3)'
     }
     res = requests.get(
         'https://www.reddit.com/r/{}/about/.json'.format(subreddit),

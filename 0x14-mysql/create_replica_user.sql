10ccb73d72ca43716602958cf033895f1d7f3d21
Alecky3 <alexmuthini3@gmail.com>
2023-10-17 11:52:19 +0300

    deletd 0x14-mysql

--- 0x14-mysql/create_replica_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'replica_user' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT SELECT ON mysql.* TO 'holberton_user'@'localhost';
-GRANT REPLICATION SLAVE ON tyrell_corp.* TO 'replica_user';
--- 0x14-mysql/create_tyrell_corp_database.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS 'tyrell_corp';
-GRANT SELECT ON tyrell.* TO 'holberton_user'@'localhost';
-CREATE TABLE IF NOT EXISTS 'nexus6';
